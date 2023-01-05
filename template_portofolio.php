7771e7675cf139692106c7fe2ccc8b05d4fc9f72
Alexandre Foulc <alexandre-foulc@orange.fr>
2023-01-05 10:37:41 +0100

    Initialisation du fichier template_portofolio

---
+++ template_portofolio.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Template Name: Page Galerie/Portofolio avec filtres
+ */
+?>
+
+<?php get_header(); ?>
+
+<main class="container text-center">
+  <!-- Texte du CHAPEAU -->
+  <section class="mx-3 mb-md-5">
+    <div class="text-center py-5 px-3 ">
+      <h2 class="lead"><?php the_field('page_chapeau'); ?></h2>
+    </div>
+  </section>
+  <!-- Texte du CHAPEAU -->
+
+  <!-- Galerie de PHOTOS -->
+  <section class="row mx-3 mb-4 g-4">
+
+
+  </section>
+  <!-- Galerie de PHOTOS -->
+</main>
+
+<?php get_footer(); ?>
